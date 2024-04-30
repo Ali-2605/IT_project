cf6455dbda463b2b6690d963f4220d54a6d9cfe6
Ali Ehab <ali.ehabezzeldin@gmail.com>
2024-04-30 18:40:10 +0300

    finished Sama

--- JavaScript/pr.js
+++ JavaScript/pr.js
@@ -7,15 +7,13 @@ function d_containsChar(str) {
 
 
 localStorage.setItem('dohajs','doha');
-    var doha = localStorage.getItem('dohajs');
-    console.log(doha); 
-}
+var doha = localStorage.getItem('dohajs');
 
 function user_login(){
     var d_user= document.getElementById('username');
     var d_pas= document.getElementById('password');
-    var d_x= user.value;
-    var d_y= pas.value;
+    var d_x= d_user.value;
+    var d_y= d_pas.value;
     if(d_x.length>=10){
         alert("write less than 10 char in username");
     }
